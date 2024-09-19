c7ea9b428a46913b16f645457adbb75e4de71088
Simeon Todorov <59865649+dinocom33@users.noreply.github.com>
2024-09-19 16:13:03 +0300

    Added change user details

--- accounts/views.py
+++ accounts/views.py
@@ -375,7 +375,7 @@ def residents(request):
 
 class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
     template_name = 'accounts/password_reset.html'
-    email_template_name = 'accounts/password_reset_email.html'
+    html_email_template_name = 'accounts/password_reset_email.html'
     subject_template_name = 'accounts/password_reset_subject'
     success_message = "We've emailed you instructions for setting your password, " \
                       "please make sure you've entered the address you registered with, and check your spam folder."
